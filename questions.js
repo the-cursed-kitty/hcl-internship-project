ee4ad6b81191d1c313cfc1a837dfadadf4ba2796
the-cursed-kitty <59267877+the-cursed-kitty@users.noreply.github.com>
2023-10-04 15:22:12 +0530

    Add files via upload

---
+++ questions.js
@@ -0,0 +1,114 @@
+// creating an array and passing the number, questions, options, and answers
+let questions = [
+  {
+    numb: 1,
+    question: "Which of the current archons is the oldest?",
+    answer: "Morax",
+    options: [
+      "Buer",
+      "Beelzbul",
+      "Morax",
+      "Barbatos"
+    ]
+  },
+  {
+    numb: 2,
+    question: "Who runs the Wangsheng Funeral Parlour?",
+    answer: "Hu Tao",
+    options: [
+      "Zhongli",
+      "Hu Tao",
+      "Ningguang",
+      "Baizhu"
+    ]
+  },
+  {
+    numb: 3,
+    question: "What martial arts group is Xingqiu in?",
+    answer: "Guhua Clan",
+    options: [
+      "Guhua Clan",
+      "Guhra Clan",
+      "Guava Clan",
+      "Guha Clan"
+    ]
+  },
+  {
+    numb: 4,
+    question: "Which character was on the first limited banner?",
+    answer: "Venti",
+    options: [
+      "Klee",
+      "Zhongli",
+      "Venti",
+      "Tartaglia"
+    ]
+  },
+  {
+    numb: 5,
+    question: "Which of these characters ISN'T obtained for free?",
+    answer: "Razor",
+    options: [
+      "Kaeya",
+      "Barbara",
+      "Xiangling",
+      "Razor"
+    ]
+  },
+  {
+    numb: 6,
+    question: "Which character's specialty food is a fried egg?",
+    answer: "Bennett",
+    options: [
+      "Rosaria",
+      "Qiqi",
+      "Venti",
+      "Bennett"
+    ]
+  },
+  {
+    numb: 7,
+    question: "Which of these areas ISN'T in Mondstadt?",
+    answer: "Sea of Clouds",
+    options: [
+      "Cape oath",
+      "Musk Reef",
+      "Sea of Clouds",
+      "Dragonspine"
+    ]
+  },
+  {
+    numb: 8,
+    question: "How many weapon types are there?",
+    answer: "5",
+    options: [
+      "4",
+      "5",
+      "6",
+      "7"
+    ]
+  },
+  {
+    numb: 9,
+    question: "Who is the Yuheng of the Liyue Qixing?",
+    answer: "Keqing",
+    options: [
+      "Keqing",
+      "Ningguang",
+      "Ganyu",
+      "Yelan"
+    ]
+  },
+  {
+    numb: 10,
+    question: "When does the spiral abyss reset?",
+    answer: "Every two weeks",
+    options: [
+      "Every month",
+      "Every week",
+      "Every two weeks",
+      "Every three weeks"
+    ]
+  }
+    
+];
